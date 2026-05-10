25f8e325a7bfe46b1099a62e8918138db89ddfa6
DaanMichielsen <michielsendaan8@hotmail.com>
2026-05-10 16:35:25 +0200

    feat(desktop): add Rajdhani + IBM Plex Sans fonts and CSS brand tokens

--- apps/desktop/src/main.tsx
+++ apps/desktop/src/main.tsx
@@ -1,3 +1,8 @@
+import '@fontsource/rajdhani/400.css'
+import '@fontsource/rajdhani/600.css'
+import '@fontsource/rajdhani/700.css'
+import '@fontsource/ibm-plex-sans/400.css'
+import '@fontsource/ibm-plex-sans/500.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
